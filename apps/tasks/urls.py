be5c6cbb107558e2ca15ecee15f2c2016116fc03
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2025-03-11 09:20:14 +0100

    added one tag get view

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.tasks.views.tag_view import TagView
+from apps.tasks.views.tag_view import TagView, TagDetailApiView
 
 urlpatterns = [
     path('tags/', TagView.as_view(), name='tags'),
+    path('tags/<int:pk>', TagDetailApiView.as_view(), name='tag'),
+
 ]
--- apps/tasks/views/tag_view.py
+++ apps/tasks/views/tag_view.py
@@ -1,6 +1,5 @@
-from gc import get_objects
-
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from apps.tasks.models import Tag
@@ -18,10 +17,21 @@ class TagView(APIView):
     def get_objects(self):
         return Tag.objects.all()
 
-
     def post(self, request):
         serializer = TagSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TagDetailApiView(APIView):
+
+    def get_object(self, pk):
+        tag = get_object_or_404(Tag, pk=pk)
+        return tag
+
+    def get(self, request, pk):
+        tag = self.get_object(pk)
+        serializer = TagSerializer(tag)
+        return Response(serializer.data, status=status.HTTP_200_OK)
